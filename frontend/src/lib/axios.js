e67eb931eadf0615707b1d79ae2f4e860ce042ef
Zaid-Khan-1756 <chmcs2026.zaid.khan@gmail.com>
2026-02-25 13:23:28 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api=axios.create({
     //baseURL:'http://localhost:3000'
-    baseURL:'https://bookstore-wp71.onrender.com'
+    baseURL:'https://mern-bookstore-opoc.onrender.com'
 })
 export default api
